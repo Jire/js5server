505f1d1cabf655a9b6924b60ead7af5b050e17ae
Jire <thomasgnappo@gmaIl.com>
2024-02-08 19:28:31 -0500

    Remove Nexus/Snapshot set to old version (it is set automatically)

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,9 +116,6 @@ nexusPublishing {
             if (false) { // only for users registered in Sonatype after 24 Feb 2021
                 nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
                 snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            } else {
-                nexusUrl.set(uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/"))
-                snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
             }
 
             val ossrhUsername = providers.environmentVariable("OSSRH_USERNAME")
