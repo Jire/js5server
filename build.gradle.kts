df3cf92afb010e7507f1211883cf31a1e9f5d811
Jire <thomasgnappo@gmaIl.com>
2024-03-14 20:04:56 -0400

    Version `1.0.2`: Updated to latest JDK `21`, Kotlin `1.9.23`, and Netty

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.jire"
-version = "1.0.1"
+version = "1.0.2"
 description = "fast simple JS5 server"
 
 application {
@@ -37,9 +37,9 @@ dependencies {
         "buffer",
         "transport-native-epoll",
         "transport-native-kqueue",
-    )) implementation("io.netty:netty-$module:4.1.106.Final")
+    )) implementation("io.netty:netty-$module:4.1.107.Final")
 
-    implementation("io.netty.incubator:netty-incubator-transport-native-io_uring:0.0.24.Final")
+    implementation("io.netty.incubator:netty-incubator-transport-native-io_uring:0.0.25.Final")
 
     implementation("it.unimi.dsi:fastutil:8.5.13")
     implementation("org.jctools:jctools-core:4.0.3")
@@ -49,7 +49,7 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 java {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
         maven(url = "https://repo.openrs2.org/repository/openrs2-snapshots/")
     }
     pluginManagement.plugins.apply {
-        kotlin("jvm").version("1.9.22")
+        kotlin("jvm").version("1.9.23")
         id("com.github.johnrengelman.shadow") version "8.1.1"
         id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-2"
     }
